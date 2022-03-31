11be024d542175476f3fb795d33fb09230e7030a
Paul <paul@theapplegates.com>
2022-03-31 14:58:45 -0400

    first commit

---
+++ site.config.js
@@ -0,0 +1,45 @@
+export default {
+  // the site's root Notion page (required)
+  rootNotionPageId: '26eeb9fc1ea24d739883ad9193dc9fd3',
+
+  // if you want to restrict pages to a single notion workspace (optional)
+  // (this should be a Notion ID; see the docs for how to extract this)
+  rootNotionSpaceId: null,
+
+  // basic site info (required)
+  name: 'Living Life',
+  domain: 'notion.paulapplegate.com',
+  author: 'Paul Applegate',
+
+  // open graph metadata (optional)
+  description: 'Example Next.js Notion Starter Kit Site',
+
+  // social usernames (optional)
+  twitter: 'transitive_bs',
+  github: 'theapplegates',
+  linkedin: '',
+
+  // default notion icon and cover images for site-wide consistency (optional)
+  // page-specific values will override these site-wide defaults
+  defaultPageIcon: null,
+  defaultPageCover: null,
+  defaultPageCoverPosition: 0.5,
+
+  // whether or not to enable support for LQIP preview images (optional)
+  isPreviewImageSupportEnabled: false,
+
+  // whether or not redis is enabled for caching generated preview images (optional)
+  // NOTE: if you enable redis, you need to set the `REDIS_HOST` and `REDIS_PASSWORD`
+  // environment variables. see the readme for more info
+  isRedisEnabled: false,
+
+  // map of notion page IDs to URL paths (optional)
+  // any pages defined here will override their default URL paths
+  // example:
+  //
+  // pageUrlOverrides: {
+  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
+  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
+  // }
+  pageUrlOverrides: null
+}
